2f0df50af395fcd46a73390670c1d116558264d7
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2019-03-09 23:57:16 +0530

    Update naive_bayes.java

--- naive_bayes.java
+++ naive_bayes.java
@@ -2,7 +2,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
